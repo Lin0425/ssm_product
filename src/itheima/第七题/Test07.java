a18d946b49247c002bdb24fa142b9233295a8fa0
xuchenngfeifei@163.com <xiaoer>
2019-03-12 19:57:13 +0800

    删除了2部分

--- src/itheima/第七题/Test07.java
+++
@@ -1,9 +0,0 @@
-package itheima.第七题;
-
-public class Test07 {
-    public static void main(String[] args) {
-        MyDate date = new MyDate(1900,1,1);
-        date.showDate();
-        date.isBi();
-    }
-}
